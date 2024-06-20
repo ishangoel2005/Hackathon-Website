15eb73a32aa8fca0e70e48094811e07b15a34765
ishangoel2005 <ishangoel2005@gmail.com>
2024-06-20 10:18:40 +0530

    contact form added

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ document.querySelector('.menu-button').addEventListener('click', function() {
 document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('.dropdown').classList.remove('show');
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('contactModal');
+    const btn = document.querySelector('.get_in_touch_bar');
+    const span = document.querySelector('.close-button');
+
+    btn.onclick = function () {
+        modal.style.display = 'flex';
+    }
+
+    span.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
